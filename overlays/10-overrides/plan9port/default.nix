6bece846165ef653be890cd9ee0666890971f9a4
Yestin L. Harrison <yestin@ylh.io>
2021-12-01 19:16:26 -0800

    plan9port: no more need for the version fix

--- overlays/10-overrides/plan9port/default.nix
+++ overlays/10-overrides/plan9port/default.nix
@@ -1,13 +1,11 @@
 self: super: let
   lib = super.lib;
   lastby = sep: x: lib.last (lib.splitString sep x);
-in super.plan9port.overrideAttrs (old@{meta, src, pname, ...}: rec {
+in super.plan9port.overrideAttrs (_old: rec {
   patches = [
     ./acme-backport-9front-spacesindent.patch
     ./pwd-import-from-4e.patch
     ./fontsrv-slightly-fudge-height-numbers.patch
     ./fontsrv-leave-vanity-behind.patch
   ];
-#  version = lastby "=" (lastby "+" old.version);
-#  name = "${pname}-${version}";
 })
