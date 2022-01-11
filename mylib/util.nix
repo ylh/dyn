3f9f90933aef95ff3a2c466c8010238d78f2872c
Yestin L. Harrison <yestin@ylh.io>
2022-01-10 18:38:38 -0800

    mylib: add cutesy optional to util

--- mylib/util.nix
+++ mylib/util.nix
@@ -5,4 +5,12 @@
   lastBy = sep: x: lib.last (lib.splitString sep x);
   overlaysIf = b: os: lib.composeManyExtensions (lib.optionals b os) (_: _: {});
   systemContains = s: elem s (split "-" currentSystem);
+  optionalOf = x: with lib; if isList x then
+    optionals
+  else if isAttrs x then
+    optionalAttrs
+  else if isString x then
+    optionalString
+  else throw ("don't know optional of type " + typeOf x);
+  optionalBy = b: x: (optionalOf x) b x;
 }
