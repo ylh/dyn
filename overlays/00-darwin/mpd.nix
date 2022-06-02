1729f87a69c7a2b214bcbd7905ac11f447593f28
Yestin L. Harrison <yestin@ylh.io>
2022-06-01 23:44:03 -0700

    overlays/darwin: rm mpd.nix

--- overlays/00-darwin/mpd.nix
+++
@@ -1,6 +0,0 @@
-self: { mpd, game-music-emu, stdenv, ... }: {
-  mpd = mpd.override { inherit stdenv; };
-  game-music-emu = game-music-emu.overrideAttrs (old: {
-    cmakeFlags = [ "-DENABLE_UBSAN=OFF" ];
-  });
-}
