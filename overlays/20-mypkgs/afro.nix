cb92267aa0949358c4cf2fe25cab48d1d68ad301
Yestin L. Harrison <yestin@ylh.io>
2022-06-13 03:21:39 -0700

    mypkgs: abstract out callPackage

--- overlays/20-mypkgs/afro.nix
+++ overlays/20-mypkgs/afro.nix
@@ -1,4 +1,7 @@
-{stdenv, lib, fetchFromGitHub, python3Packages}: python3Packages.buildPythonPackage rec {
+{ buildPythonPackage, fetchFromGitHub
+, numpy, colorlog, kaitaistruct
+}:
+buildPythonPackage rec {
   pname = "afro";
   version = "0.2";
 
@@ -8,5 +11,6 @@
     rev = "d24e7e1c722ba51f38bb7accbb171b33e3165b45";
     sha256 = "0khzwkhdh68hhc4v8ig2m2rm2vbzvjs085kl5fdl34dz4r9dvkn4";
   };
-  propagatedBuildInputs = with python3Packages; [numpy colorlog kaitaistruct setuptools];
+
+  propagatedBuildInputs = [ numpy colorlog kaitaistruct ];
 }
--- overlays/20-mypkgs/default.nix
+++ overlays/20-mypkgs/default.nix
@@ -1,12 +1,16 @@
 self: super: let
   frameworks = super.darwin.apple_sdk.frameworks;
   special = {
-    osxutils = {
+    osxutils.args = {
       inherit (frameworks) Carbon Cocoa;
     };
-    daemondo = {
+    daemondo.args = {
       inherit (frameworks) CoreFoundation SystemConfiguration IOKit;
     };
+    afro.caller = super.python3Packages;
   };
-  call = (n: v: super.callPackage v (special.${n} or {}));
+  call = (n: v:
+    (special.${n}.caller or super).callPackage
+    v
+    (special.${n}.args or {}));
 in super.lib.hereFlatAttrsMap call ./.
