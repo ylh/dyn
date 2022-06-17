34441c10e12533c8fd6a59c5cec149ddc77d0fce
Yestin L. Harrison <yestin@ylh.io>
2022-06-16 18:19:48 -0700

    overlays/plan9port: devdraw middle click

--- overlays/10-patches/plan9port/default.nix
+++ overlays/10-patches/plan9port/default.nix
@@ -1,5 +1,6 @@
 self: { plan9port, ... }: plan9port.overrideAttrs ({ patches ? [], ... }: {
   patches = patches ++ [
+    ./devdraw-3-finger-middle-click.patch
     ./acme-backport-9front-spacesindent.patch
     ./pwd-import-from-4e.patch
     ./fontsrv-slightly-fudge-height-numbers.patch
