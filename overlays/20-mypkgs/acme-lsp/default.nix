8d82d4f1908d90f0ecaf2c604ea1b1db9e987333
Yestin L. Harrison <yestin@ylh.io>
2022-12-04 02:02:51 -0800

    mypkgs: rm gomod2nix

--- overlays/20-mypkgs/acme-lsp/default.nix
+++ overlays/20-mypkgs/acme-lsp/default.nix
@@ -1,5 +1,5 @@
-{ buildGoApplication, fetchFromGitHub}:
-buildGoApplication rec {
+{ buildGoModule, fetchFromGitHub }:
+buildGoModule rec {
   pname = "acme-lsp";
   version = "2022-05-03";
 
@@ -9,7 +9,8 @@ buildGoApplication rec {
     rev = "f95a8c546675204b20be5e52bd747f2e20fda308";
     sha256 = "1ycd20myyg7434ycg8ahihydgb4byq1gm0qqxc2p09j793yskvxg";
   };
-  modules = ./gomod2nix.toml;
+
+  vendorSha256 = "1a99759x8jccajrral65d8s4cc9yn68qg56k3qm676vb2v0mfalv";
 
   doCheck = false; # wants pyls which we can't build
 }
